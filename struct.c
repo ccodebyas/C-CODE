12209a77df069738882aa22c7e01908dd4a72120
ccodebyas <ccodebyas@gmail.com>
2026-03-16 17:56:12 +0500

    struct

--- struct.c
+++ struct.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 	struct Students {
@@ -8,14 +9,18 @@ int main()
 	};
 	// pala
 	struct Students pala;
+	strcpy(pala.name,"AS");
 	pala.age = 17;
 	pala.marks = 92.8;
-	printf("%d",pala.age);
+	printf("%s",pala.name);
+	printf("\n%d",pala.age);
 	printf("\n%.2f",pala.marks);
     // dosra
     struct Students dosra ;
+    strcpy(dosra.name,"SA");
     dosra.age = 18;
     dosra.marks = 92.9;
+    printf("\n%s",dosra.name);
     printf("\n%d",dosra.age);
     printf("\n%.2f",dosra.marks);
     
